591d92c6e06c80d2343eb85456223f1032a5e9c9
Zachary Carter <carterza@gmail.com>
2017-03-20 11:35:42 -0400

    Forgot to comment out android_egl_context_backup call.

--- src/video/android/SDL_androidevents.c
+++ src/video/android/SDL_androidevents.c
@@ -75,7 +75,7 @@ Android_PumpEvents(_THIS)
 #if SDL_ANDROID_BLOCK_ON_PAUSE
     if (isPaused && !isPausing) {
         /* Make sure this is the last thing we do before pausing */
-        android_egl_context_backup();
+        // android_egl_context_backup();
         ANDROIDAUDIO_PauseDevices();
         if(SDL_SemWait(Android_ResumeSem) == 0) {
 #else
