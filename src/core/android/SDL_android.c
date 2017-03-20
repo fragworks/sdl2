f80b5b50bd367f08bad0a33507720ec7fab395e8
Zachary Carter <carterza@gmail.com>
2017-03-20 11:48:36 -0400

    Removing create / destroy EGL Surface calls in SDL_android.c

--- src/core/android/SDL_android.c
+++ src/core/android/SDL_android.c
@@ -344,7 +344,7 @@ JNIEXPORT void JNICALL SDL_JAVA_INTERFACE(onNativeSurfaceChanged)(JNIEnv* env, j
             ANativeWindow_release(data->native_window);
         }
         data->native_window = Android_JNI_GetNativeWindow();
-        data->egl_surface = SDL_EGL_CreateSurface(_this, (NativeWindowType) data->native_window);
+        // data->egl_surface = SDL_EGL_CreateSurface(_this, (NativeWindowType) data->native_window);
     }
     
     /* GL Context handling is done in the event loop because this function is run from the Java thread */
@@ -370,7 +370,7 @@ JNIEXPORT void JNICALL SDL_JAVA_INTERFACE(onNativeSurfaceDestroyed)(JNIEnv* env,
     
     if (data->egl_surface != EGL_NO_SURFACE) {
         SDL_EGL_MakeCurrent(_this, NULL, NULL);
-        SDL_EGL_DestroySurface(_this, data->egl_surface);
+        // SDL_EGL_DestroySurface(_this, data->egl_surface);
         data->egl_surface = EGL_NO_SURFACE;
     }
     
